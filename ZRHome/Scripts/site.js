72db31452e2ea4d9dbb49d50ec7112e455b9d2e4
zubinraj <mangachalil@gmail.com>
2013-09-13 13:50:26 +0530

    Updated style

---
+++ ZRHome/Scripts/site.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+
+    var $container = $("#gallery-container");
+
+    $container.isotope({
+        layoutMode: 'fitRows',
+        itemSelector: '.gallery-item',
+        animationEngine: 'best-available'
+        //masonry: { columnWidth: $container.width() / 3 }
+    });
+
+    $("#filters a").click(function () {
+        var selector = $(this).attr('rel');
+        $container.isotope({ filter: selector });
+        //$container.isotope({ sortBy: 'desc' });
+    });
+
+});
+
