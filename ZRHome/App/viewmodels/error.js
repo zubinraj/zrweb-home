838cfbc6b57759f6b9a0b732af40be28ef402def
zubinraj <mangachalil@gmail.com>
2013-09-17 16:19:38 +0530

    Added error route

---
+++ ZRHome/App/viewmodels/error.js
@@ -0,0 +1,10 @@
+﻿define(['plugins/http', 'knockout', 'services/logger'], function (http, ko, logger) {
+
+    var ctor = {
+        title: 'Error',
+        msg: 'Oops! Something went wrong. Please try again.'
+    }
+
+    return ctor;
+
+});
--- ZRHome/App/viewmodels/shell.js
+++ ZRHome/App/viewmodels/shell.js
@@ -14,10 +14,11 @@
         });
 
         router.map([
-            { route: '',        title: 'Welcome',       icon: 'glyphicon-home',     moduleId: 'viewmodels/welcome', nav: true },
-            { route: 'blog',    title: 'Blog',          icon: 'glyphicon-book',     moduleId: 'viewmodels/blog',    nav: true },
-            { route: 'photos',  title: 'Photography',   icon: 'glyphicon-camera',   moduleId: 'viewmodels/photos',  nav: true },
-            { route: 'contact', title: 'Contact',       icon: 'glyphicon-envelope', moduleId: 'viewmodels/contact', nav: true }
+            { route: '', title: 'Welcome', icon: 'glyphicon-home', moduleId: 'viewmodels/welcome', nav: true },
+            { route: 'blog', title: 'Blog', icon: 'glyphicon-book', moduleId: 'viewmodels/blog', nav: true },
+            { route: 'photos', title: 'Photography', icon: 'glyphicon-camera', moduleId: 'viewmodels/photos', nav: true },
+            { route: 'contact', title: 'Contact', icon: 'glyphicon-envelope', moduleId: 'viewmodels/contact', nav: true },
+            { route: 'error', title: 'Error', icon: '', moduleId: 'viewmodels/error', nav: false }
         ]).buildNavigationModel();
 
 
