c893be8e1c8a6b7dc43d0938c9432cedf3d39b27
zubinraj <mangachalil@gmail.com>
2013-09-22 23:54:33 +0530

    Referting the changes made to load all external js and css from cdn. Now only bootstrap loads from cdn as it has issues while bundling.

--- ZRHome/App_Start/DurandalBundleConfig.cs
+++ ZRHome/App_Start/DurandalBundleConfig.cs
@@ -9,17 +9,19 @@ namespace ZRHome {
 
 	  bundles.Add(
 		new ScriptBundle("~/Scripts/js")
-			//.Include("~/Scripts/jquery-{version}.js")
-			//.Include("~/Scripts/bootstrap.js")
-			//.Include("~/Scripts/knockout-{version}.js")
-            //.Include("~/Scripts/jquery.isotope.js")
-            //.Include("~/Scripts/toastr.js")
-            //.Include("~/Scripts/jquery.lazyload.js")
+			.Include("~/Scripts/jquery-{version}.js")
+              // Commented by ZR: Ignore bootstrap.css as it has relative path issues while bundling 
+              // This is included in index.cshtml
+              //.Include("~/Scripts/bootstrap.js")
+			.Include("~/Scripts/knockout-{version}.js")
+            .Include("~/Scripts/jquery.isotope.js")
+            .Include("~/Scripts/toastr.js")
+            .Include("~/Scripts/jquery.lazyload.js")
             ////.Include("~/Scripts/site.js")
-            //.Include("~/Scripts/jquery.fancybox.js")
-            //.Include("~/Scripts/jquery.fancybox-buttons.js")
-            //.Include("~/Scripts/jquery.fancybox-media.js")
-            //.Include("~/Scripts/jquery.fancybox-thumbs.js")
+            .Include("~/Scripts/jquery.fancybox.js")
+            .Include("~/Scripts/jquery.fancybox-buttons.js")
+            .Include("~/Scripts/jquery.fancybox-media.js")
+            .Include("~/Scripts/jquery.fancybox-thumbs.js")
 
 
         );
@@ -27,19 +29,19 @@ namespace ZRHome {
       bundles.Add(
         new StyleBundle("~/Content/css")
           .Include("~/Content/ie10mobile.css")
-          // Commented by ZR: Ignore bootstrap.css as it has relative path issues while bundling 
-          // This is included in index.cshtml
-          //.Include("~/Content/bootstrap/bootstrap.css")       
-          //.Include("~/Content/bootstrap/bootstrap-theme.css")
-          //.Include("~/Content/font-awesome.css")
+            // Commented by ZR: Ignore bootstrap.css as it has relative path issues while bundling 
+            // This is included in index.cshtml
+            //.Include("~/Content/bootstrap/bootstrap.css")       
+            //.Include("~/Content/bootstrap/bootstrap-theme.css")
+          .Include("~/Content/font-awesome.css")
 		  .Include("~/Content/durandal.css")
           .Include("~/Content/starterkit.css")
           .Include("~/Content/site.css")
           .Include("~/Content/isotope.css")
-          //.Include("~/Content/toastr.css")
-          //.Include("~/Content/jquery.fancybox.css")
-          //.Include("~/Content/jquery.fancybox-buttons.css")
-          //.Include("~/Content/jquery.fancybox-thumbs.css")
+          .Include("~/Content/toastr.css")
+          .Include("~/Content/jquery.fancybox.css")
+          .Include("~/Content/jquery.fancybox-buttons.css")
+          .Include("~/Content/jquery.fancybox-thumbs.css")
 );
     }
 
