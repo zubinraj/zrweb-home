c2471ed46cc1cabfaffbfd2e8819e1e6231495cf
zubinraj <mangachalil@gmail.com>
2015-09-18 07:48:43 +0530

    Bump version

--- ZRHome/App/main.js
+++ ZRHome/App/main.js
@@ -1,5 +1,5 @@
 ﻿requirejs.config({
-    urlArgs: "version=1.3.2.1",
+    urlArgs: "version=1.4",
     //urlArgs: "bust=" + (new Date().getTime()),   // cache buster to load new version while debugging. Todo: comment out defore deployment
     paths: {
         'text': '../Scripts/text',
